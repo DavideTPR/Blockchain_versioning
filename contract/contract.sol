54f05dcf045adac92c21b2046d988fe1d4d4e465
davide.tarasconi@studenti.unipr.it <DavideTPR>
2019-04-08 17:03:02 +0200

    V_0.0.1
    Start develop the contract

--- contract/contract.sol
+++ contract/contract.sol
@@ -1,10 +1,42 @@
 pragma solidity >=0.5.0 <0.7.0;
 
+///@title Versioning of a document
+///@author Davide Tarasconi
+///@notice Smart contract that allow to save information about a version of a document
 contract Versioning{
 
-    mapping (address => uint) document; //document's hash
-    address public creator; //document's creator
-    address public mod_user; //user who made last modify
-    uint public date_mod; //last modify
+    Document doc;
+    mapping (uint256 => Document[]) modify;
+    //event to notify the modification of a document
+    event changeDocument(address mod, uint doc);
 
+
+    constructor(uint256 doc_h) public{
+        doc = new Document(msg.sender, doc_h, 0);
+    }
+
+
+    function change(uint256 doc_h) public{
+        require(doc.value != doc_h);
+        modify[doc.creator].push(new Document(msg.sender, doc_h, doc.version+1));
+    }
+
+}
+
+///@title document
+contract Document{
+    address public creator; //document's creator
+    uint256 public value; //document's hash
+    uint public creation; //date of creation of this document
+    uint8 public version; //version of the document
+
+
+    ///@param c addres of creator
+    ///@param hash_doc the identificator of the document
+    constructor(address c, uint256 hash_doc, uint8 v) public{
+        creator = c;
+        value = hash_doc;
+        creation = now;
+        version = v;
+    }
 }
